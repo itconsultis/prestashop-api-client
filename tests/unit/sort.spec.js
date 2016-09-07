0b016ec14cd65e4223acd24bb59ceeac9d2338bc
Rich Choy <rich@it-consultis.com>
2016-09-07 13:52:52 +0800

    sort test

--- tests/unit/sort.spec.js
+++ tests/unit/sort.spec.js
@@ -14,13 +14,14 @@ describe('sort', () => {
     });
 
     it('comparator sorts in ascending order', () => {
-      let values = [{prop: 1}, {prop: 2}];
+      let values = [{prop: 3}, {prop: 1}, {prop: 2}];
       let evaluate = value => value.prop;
       let comparator = sort.ascending(evaluate);
       let sorted = values.sort(comparator);
 
       expect(sorted[0].prop).to.equal(1);
       expect(sorted[1].prop).to.equal(2);
+      expect(sorted[2].prop).to.equal(3);
     });
   });
 
@@ -35,13 +36,14 @@ describe('sort', () => {
     });
 
     it('comparator sorts in descending order', () => {
-      let values = [{prop: 1}, {prop: 2}];
+      let values = [{prop: 3}, {prop: 1}, {prop: 2}];
       let evaluate = value => value.prop;
       let comparator = sort.descending(evaluate);
       let sorted = values.sort(comparator);
 
-      expect(sorted[0].prop).to.equal(2);
-      expect(sorted[1].prop).to.equal(1);
+      expect(sorted[0].prop).to.equal(3);
+      expect(sorted[1].prop).to.equal(2);
+      expect(sorted[2].prop).to.equal(1);
     });
   });
 });
