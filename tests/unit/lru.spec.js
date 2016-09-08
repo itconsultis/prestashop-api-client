a1fe0833d4f13b6098e493ac62dbc47581d22c38
Rich Choy <rich@it-consultis.com>
2016-09-08 16:57:34 +0800

    rename cache spec -> lru.spec.js

--- tests/unit/lru.spec.js
+++ tests/unit/lru.spec.js
@@ -3,6 +3,24 @@ import LRUCache from 'lru-cache';
 
 describe('lru', () => {
 
+  describe('.dummy()', () => {
+    it('returns an null object that duck-types LRUCache', () => {
+      let dummy = lru.dummy();
+      expect(dummy.length).to.equal(0);
+      expect(dummy.itemCount).to.equal(0);
+      expect(dummy.get).to.be.a('function');
+      expect(dummy.set).to.be.a('function');
+      expect(dummy.reset).to.be.a('function');
+      expect(dummy.peek).to.be.a('function');
+      expect(dummy.del).to.be.a('function');
+      expect(dummy.has).to.be.a('function');
+      expect(dummy.forEach).to.be.a('function');
+      expect(dummy.keys).to.be.a('function');
+      expect(dummy.values).to.be.a('function');
+      expect(dummy.dump).to.be.a('function');
+    });
+  });
+
   describe('.instance()', () => {
 
     beforeEach(() => lru.flush());
