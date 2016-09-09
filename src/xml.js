faa93ded177823564a6a8c99d2c2eebe1a2ed1c9
Rich Choy <rich@it-consultis.com>
2016-09-09 13:09:06 +0800

    huge refactor of xml module
    - add text() helper function
    - add attr() helper function

--- src/xml.js
+++ src/xml.js
@@ -23,6 +23,40 @@ export const parse = xml.parse = (xml) => {
 };
 
 
+/**
+ * Extract the text value from a node 
+ * @param mixed input
+ * @return {String}
+ */
+const text = (input) => {
+  let raw = Array.isArray(input) ? input[0] : input;
+
+  if (typeof raw == 'object') {
+    raw = raw._ || '';
+  }
+
+  return String(raw).trim();
+};
+
+/**
+ * Extract an attribute value on a node
+ * @param {mixed} input
+ * @return {String}
+ */
+const attr = (input, name) => {
+  let raw = input;
+  
+  if (Array.isArray(raw)) {
+    raw = raw[0];
+  } 
+
+  if (raw.$ && raw.$[name] !== undefined) {
+    raw = raw.$[name];
+  }
+
+  return String(raw).trim();
+};
+
 ////////////////////////////////////////////////////////////////////////////////
 
 parse.model = {};
@@ -40,7 +74,7 @@ parse.model.ids = (xml, api, nodetype) => {
 
   .then((obj) => {
     let list = obj.prestashop[api][0][nodetype];
-    return list.map((obj) => integer(obj.$.id));
+    return list.map((obj) => integer(attr(obj, 'id')));
   });
 };
 
@@ -59,7 +93,7 @@ parse.product.properties = (xml, language=1) => {
   .then((obj) => {
     let base = obj.prestashop.product[0];
     let assocs = base.associations[0];
-    let lang = (attr) => attr.$.id == language;
+    let lang = (obj) => attr(obj, 'id') == language;
 
     let names = base.name[0].language;
     let descs = base.description[0].language;
@@ -68,16 +102,16 @@ parse.product.properties = (xml, language=1) => {
     let images = assocs.images[0].image;
 
     return {
-      'id': base.id[0].trim(),
-      'name': (names.filter(lang).pop()._||'').trim(),
-      'description': (descs.filter(lang).pop()._||'').trim(),
-      'description_short': (shortdescs.filter(lang).pop()._||'').trim(),
-      'price': base.price[0].trim(),
-      'available_for_order': base.available_for_order[0].trim(),
+      'id': text(base.id),
+      'name': text(names.filter(lang).pop()),
+      'description': text(descs.filter(lang).pop()),
+      'description_short': text(shortdescs.filter(lang).pop()),
+      'price': text(base.price),
+      'available_for_order': text(base.available_for_order),
       'related': {
-        'manufacturer': integer((base.id_manufacturer[0]||'').trim()),
-        'combinations': combos.map(combo => integer(combo.id[0].trim())),
-        'images': images.map(image => integer((image.id[0]||'').trim())),
+        'manufacturer': integer(text(base.id_manufacturer)),
+        'combinations': combos.map(combo => integer(text(combo.id))),
+        'images': images.map(image => integer(text(image.id))),
       },
     };
   });
@@ -152,6 +186,7 @@ parse.image.properties = (xml) => {
 };
 
 ////////////////////////////////////////////////////////////////////////////////
+
 parse.manufacturer = {};
 
 parse.manufacturer.properties = (xml, language=1) => {
@@ -159,56 +194,42 @@ parse.manufacturer.properties = (xml, language=1) => {
 
   .then((obj) => {
     let base = obj.prestashop.manufacturer[0];
-    let lang = (attr) => attr.$.id == language;
+    let lang = (obj) => attr(obj, 'id') == language;
     let descs = base.description[0].language;
 
     return {
-      'id': base.id[0].trim(),
-      'active': base.active[0].trim(),
-      'name': base.name[0].trim(),
-      'description': (descs.filter(lang).pop()._||'').trim(),
+      'id': text(base.id),
+      'active': text(base.active),
+      'name': text(base.name),
+      'description': text(descs.filter(lang).pop()),
     };
   })
 };
 
+////////////////////////////////////////////////////////////////////////////////
 
 parse.stock_available = {};
-
-parse.stock_available.ids = (xml, api, nodetype) => {
-  return parse(xml)
-
-  .then((obj) => {
-    let list = obj.prestashop.stock_availables[0].stock_available;
-    return list.map((obj) => integer(obj.$.id));
-  })
-};
 
 parse.stock_available.properties = (xml, language=1) => {
   return parse(xml)
 
   .then((obj) => {
     let base = obj.prestashop.stock_available[0];
-    let {id_product_attribute} = base;
-
-    if (typeof id_product_attribute[0] === 'string') {
-      id_product_attribute = id_product_attribute[0].trim();
-    }
-    else {
-      id_product_attribute = id_product_attribute[0]._.trim();
-    }
 
     return {
-      'id': base.id[0].trim(),
-      'id_product': base.id_product[0]._.trim(),
-      'id_product_attribute': id_product_attribute,
-      'id_shop': base.id_shop[0]._.trim(),
-      'id_shop_group': base.id_shop_group[0].trim(),
-      'quantity': base.quantity[0].trim(),
-      'depends_on_stock': base.depends_on_stock[0].trim(),
-      'out_of_stock': base.out_of_stock[0].trim(),
+      'id': text(base.id),
+      'id_product': text(base.id_product),
+      'id_product_attribute': text(base.id_product_attribute),
+      'id_shop': text(base.id_shop),
+      'id_shop_group': text(base.id_shop_group),
+      'quantity': text(base.quantity),
+      'depends_on_stock': text(base.depends_on_stock),
+      'out_of_stock': text(base.out_of_stock),
     };
   })
 };
+
+////////////////////////////////////////////////////////////////////////////////
 
 parse.product_option_value = {};
 
@@ -218,14 +239,14 @@ parse.product_option_value.properties = (xml, language=1) => {
   .then((obj) => {
     let base = obj.prestashop.product_option_value[0];
     let names = base.name[0].language;
-    let lang = (attr) => attr.$.id == language;
+    let lang = (obj) => attr(obj, 'id') == language;
 
     return {
-      'id': base.id[0].trim(),
-      'id_attribute_group': base.id_attribute_group[0]._.trim(),
-      'color': base.color[0].trim(),
-      'position': base.position[0].trim(),
-      'name': (names.filter(lang).pop()._||'').trim(),
+      'id': text(base.id),
+      'id_attribute_group': text(base.id_attribute_group),
+      'color': text(base.color),
+      'position': text(base.position),
+      'name': text(names.filter(lang).pop()),
     };
   })
 };
